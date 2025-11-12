2ed6d7d5d9c68b6b78947cfe1d186d6b62c67fe0
anollex <r.mih99@gmail.com>
2025-11-12 23:26:56 +0200

    boilerplate

--- main.c
+++ main.c
@@ -1,6 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#define INFECTED_DURATION 3
+#define IMMUNE_DURATION 5
+
+// #define DEBUG 1
+
+typedef struct {
+    int x;
+    int y;
+}Coordinate;
+
+typedef struct {
+    int personID;
+    Coordinate coord;
+    int status;
+    int nextStatus;
+    int statusDuration;
+    int movementDirection;
+    int movementAmplitude;
+    int infectionCounter;
+}Person;
+
+typedef enum {
+    INFECTED,
+    SUSCEPTIBLE,
+    IMMUNE
+}Status;
+
+typedef enum {
+    NORTH,  //00
+    SOUTH,  //01
+    EAST,   //10
+    WEST    //11 ^ 0x0001 = 10
+}Directions;
+
+void personScan(char* path, Person* person);
+void updateLocation(Person person);
+void computeNextStatus(Person person);
+void updateStatus(Person person);
+
+
+int main(int argc, const char * argv[]) {
+    Person person[10];
+    int maxXCoord = 0;
+    int maxYCoord = 0;
+    int numOfPersons = 0;
+    int threadCount = atoi(argv[3]);
+    int simulationTime = atoi(argv[1]);
+    char* path = argv[2];
+
+
     return 0;
+}
+
+/*-----------------------------------------------------------------
+ * Function:  Person Scan
+ * Purpose:   Input the date of all person
+ * In args:   path, person
+ */
+void personScan(char* path, Person* person) {
+
+}
+
+/*-----------------------------------------------------------------
+ * Function:  Person Print
+ * Purpose:   Output the data  of all persons
+ * In args:   path, person
+ */
+void personPrint(char* path, Person* person) {
+
+}
+
+/*-----------------------------------------------------------------
+ * Function:  Update Location
+ * Purpose:   Update the location of a person and take care of out of test area
+ * In args:   person
+ */
+void updateLocation(Person person) {
+
+}
+
+/*-----------------------------------------------------------------
+ * Function:  Compute NextStatus
+ * Purpose:   Compute the next status for a person
+ * In args:   person
+ */
+void computeNextStatus(Person person) {
+
+}
+
+/*-----------------------------------------------------------------
+ * Function:  Update Status
+ * Purpose:   Update the status to next status of a persons
+ * In args:   person
+ */
+void updateStatus(Person person) {
+
 }
